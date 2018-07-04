bd9f005b6f708f0ba940fbc6c8769a524bbdfa62
erccanuca <euca@gtu.edu.tr>
2018-07-04 14:15:26 +0300

    First upload.

---
+++ src/main/java/com/mycompany/seleniummaventestotomasyon/MainDemoClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.seleniummaventestotomasyon;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * @author ercan
+ */
+public class MainDemoClass {
+    
+    private static WebDriver driver = null;
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
